69180131e999460a99066a1608042bc76135452a
= <atid.ssri@gmail.com>
2020-12-23 17:10:32 +0700

    Update style and section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,31 @@
 import React from 'react'
+// import styled from 'styled-components'
 import home1 from '../img/home1.png'
+import {About, Description, Image, Hide} from '../Styles'
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
-                        <h2>We wirk to make</h2>
-                    </div>
-                    <div className="hide">
+                    <Hide>
+                        <h2>We work to make</h2>
+                    </Hide>
+                    <Hide>
                         <h2>
                             your <span>dearms</span> come
                         </h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contract us for any photography or videography ideas that you have professionals with amazing skills.</p>
                 <button>Contract Us</button>
-            </div>
-            <div className="imgage">
+            </Description>
+            <Image>
                 <img src={home1} alt="guy with a camara"/>
-            </div>
-        </div>
+            </Image>
+        </About>
     )
 }
 
