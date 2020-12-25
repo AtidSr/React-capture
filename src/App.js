937eae2197c1c7f0c9f64587c11257a60bf73513
= <atid.ssri@gmail.com>
2020-12-25 14:29:57 +0700

    Add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import AboutUs from './pages/AboutUs'
 import GlobalStyle from './components/GlobalStyle'
+import Nav from './components/Nav'
+import ContractUs from './pages/ContractUs'
+import OurWork from './pages/OurWorks'
+import {Switch, Route} from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
-      <AboutUs/>
+      <Nav/>
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs/>
+        </Route>
+        <Route path="/work">
+          <OurWork/>
+        </Route>
+        <Route path="/contract">
+          <ContractUs/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/ContractUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContractUs = () => {
+    return (
+        <div>
+            <h1>Our Contract</h1>
+        </div>
+    )
+}
+
+export default ContractUs
