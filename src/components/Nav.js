748b28e1307f80f6855832ad5e3a4049cb97990f
Atid Sr <atid.sr@ku.th>
2021-01-09 21:35:43 +0700

    add responsive and some animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom'
+import {motion} from 'framer-motion'
 
 const Nav = () => {
+    const {pathname} = useLocation()
     return (
         <StyleNav>
             <h1><Link id='logo' to="/">Capture</Link></h1>
             <ul>
                 <li>
                     <Link to="/">1. About Us</Link>
+                    <Line transition={{duration: 0.75}} initial={{width: '0%'}} animate={{width: pathname === '/' ? '50%': '0%'}}/>
                 </li>
                 <li>
                     <Link to="/work">2. Our Work</Link>
+                    <Line transition={{duration: 0.75}} initial={{width: '0%'}} animate={{width: pathname === '/work' ? '50%': '0%'}}/>
                 </li>
                 <li>
                     <Link to="/contract">3. Contract Us</Link>
+                    <Line transition={{duration: 0.75}} initial={{width: '0%'}} animate={{width: pathname === '/contract' ? '50%': '0%'}}/>
                 </li>
             </ul>
         </StyleNav>
@@ -51,6 +56,35 @@ const StyleNav = styled.nav`
         padding-left: 10rem;
         position: relative;
     }
+
+    @media (max-width: 1300px) {
+        flex-direction: column;
+        padding: 2rem 1rem;
+        #logo {
+            display: inline-block;
+            margin: 1rem;
+        }
+        ul {
+            padding: 2rem;
+            justify-content: space-around;
+            width: 100%;
+            li {
+                padding: 0;
+            }
+        }
+    }
+`
+
+const Line = styled(motion.div)`
+    height: 0.3rem;
+    background: #23d997;
+    width: 0%;
+    position: absolute;
+    bottom: -80%;
+    left: 60%;
+    @media (max-width: 1300px){
+        left: 0%;
+    } 
 `
 
 export default Nav
